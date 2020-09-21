cc80c4b19fe6a7e442e9e8421580c949a68121e5
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2020-09-21 16:25:36 +0900

    views.py에 menu.menu_name 추가

--- subserve/menu/views.py
+++ subserve/menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def detail(request):
+    menus=Menu.objects.all()
+    menu=Menu()
+    menu.menu_name = request.POST.get('menu_name')
+    return render(request, 'detail.html', {'menus':menus})
